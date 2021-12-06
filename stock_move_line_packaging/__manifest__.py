1b71a07df655eee1a5449c61404af3500d380a41
Janik von Rotz <login@janikvonrotz.ch>
2021-12-06 18:38:30 +0100

    new submodule stock_move_line_packaging

---
+++ stock_move_line_packaging/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Stock Move Line Packaging",
+    "summary": """
+        Link product packaging from sale order.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Inventory",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["stock", "sale"],
+    "data": ["views/view_picking_form.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
