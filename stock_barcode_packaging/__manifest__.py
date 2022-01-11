7ca35a2b768f3f6336f3f2a4e9e09d97fea9323d
Janik von Rotz <login@janikvonrotz.ch>
2022-01-11 18:10:45 +0100

    bugfix

--- stock_barcode_packaging/__manifest__.py
+++ stock_barcode_packaging/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Mint System GmbH, Odoo Community Association (OCA)",
     "website": "https://www.mint-system.ch",
     "category": "Inventory",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "license": "AGPL-3",
     "depends": ["stock_barcode", "stock_move_line_packaging"],
     "qweb": ["static/src/xml/stock_barcode_lines_template.xml"],
