2863a5b9179b22bf665598e4d6f77427aa92395e
janikvonrotz <contact@janikvonrotz.ch>
2022-04-11 14:39:53 +0200

    update readmes

--- stock_move_line_position/__manifest__.py
+++ stock_move_line_position/__manifest__.py
@@ -1,24 +1,17 @@
 {
-    'name': "Stock Move Line Position",
-
-    'summary': """
+    "name": "Stock Move Line Position",
+    "summary": """
         Get line position from purchase or sale order.
     """,
-    
-    'author': 'Mint System GmbH, Odoo Community Association (OCA)',
-    'website': 'https://www.mint-system.ch',
-    'category': 'Inventory',
-    'version': '14.0.1.1.0',
-    'license': 'AGPL-3',
-    
-    'depends': ['stock', 'sale_order_line_pos', 'purchase_order_line_position'],
-
-    'data': [
-        'views/view_picking_form.xml',
-    ],
-
-    'installable': True,
-    'application': False,
-    'auto_install': False,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Inventory",
+    "version": "14.0.1.1.0",
+    "license": "AGPL-3",
+    "depends": ["stock", "sale_order_line_pos", "purchase_order_line_position"],
+    "data": ["views/view_picking_form.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
     "images": ["images/screen.png"],
 }
