e28730cdcd73fa2ec87b4baf5b30d0099abdef74
janikvonrotz <contact@janikvonrotz.ch>
2022-06-07 10:06:41 +0200

    [stock_move_line_position] enable fallback pos

--- stock_move_line_position/models/stock_move.py
+++ stock_move_line_position/models/stock_move.py
@@ -9,10 +9,15 @@ class StockMove(models.Model):
     position = fields.Integer("Pos", compute='_compute_get_position')
 
     def _compute_get_position(self):
+        """Get position from sale or purchase order"""
         for rec in self:
             if rec.picking_id.sale_id:
                 rec.position = rec.picking_id.sale_id.get_position(rec.product_id, rec.product_uom_qty)
+                if rec.position == 0:
+                    rec.position = rec.picking_id.sale_id.get_position(rec.product_id)
             elif rec.picking_id.purchase_id:
                 rec.position = rec.picking_id.purchase_id.get_position(rec.product_id, rec.product_uom_qty)
+                if rec.position == 0:
+                    rec.position = rec.picking_id.purchase_id.get_position(rec.product_id)
             else:
                 rec.position = 0
