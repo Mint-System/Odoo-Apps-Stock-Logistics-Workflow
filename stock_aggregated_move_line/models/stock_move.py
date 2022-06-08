1c0f6e1be4c6c5b25b2190d1fc3cf9c390663407
janikvonrotz <contact@janikvonrotz.ch>
2022-06-08 11:40:39 +0200

    [stock_aggregated_move_line] fix

--- stock_aggregated_move_line/models/stock_move.py
+++ stock_aggregated_move_line/models/stock_move.py
@@ -7,9 +7,14 @@ class StockMoveLine(models.Model):
     _inherit = 'stock.move.line'
 
     def _get_aggregated_product_quantities(self, **kwargs):
+        """Retrieve move line using product and qty done."""
         aggregated_move_lines = super()._get_aggregated_product_quantities(**kwargs)
+        # _logger.warning(aggregated_move_lines)
         for aggregated_move_line in aggregated_move_lines:
             product = aggregated_move_lines[aggregated_move_line]['product']
-            aggregated_move_lines[aggregated_move_line]['move_line'] = self.filtered(lambda l: l.product_id == product)
-        # _logger.warning(aggregated_move_lines)
+            move_lines = self.filtered(lambda l: l.product_id == product)
+            if move_lines:
+                aggregated_move_lines[aggregated_move_line]['move_line'] = move_lines[0]
+            else:
+                aggregated_move_lines[aggregated_move_line]['move_line'] = False
         return aggregated_move_lines
--- stock_aggregated_move_lines_description/models/stock_move.py
+++ stock_aggregated_move_lines_description/models/stock_move.py
@@ -1,15 +1,16 @@
 import logging
 _logger = logging.getLogger(__name__)
 from odoo import _, api, fields, models
-
+import ast
 
 class StockMoveLine(models.Model):
     _inherit = 'stock.move.line'
 
     def _get_aggregated_product_quantities(self, **kwargs):
         aggregated_move_lines = super()._get_aggregated_product_quantities(**kwargs)
+        # _logger.warning(aggregated_move_lines)
         for aggregated_move_line in aggregated_move_lines:
             move_line = aggregated_move_lines[aggregated_move_line]['move_line']
-            aggregated_move_lines[aggregated_move_line]['description'] = move_line.move_id.description_picking.replace('\n', '<br/>')
-        # _logger.warning(aggregated_move_lines)
+            if move_line:
+                aggregated_move_lines[aggregated_move_line]['description'] = move_line.move_id.description_picking.replace('\n', '<br/>')
         return aggregated_move_lines
