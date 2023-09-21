96c3a47cc21a0169c70b1fa8abfc4cb4e6e989ed
Janik von Rotz <login@janikvonrotz.ch>
2023-09-21 09:49:48 +0200

    [stock_lot_traceability_list] init

---
+++ stock_lot_traceability_list/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Stock Lot Traceability List",
+    "summary": """
+        Show nested traceability report data as list.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Inventory",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["stock"],
+    "data": ["views/stock_lot.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}
