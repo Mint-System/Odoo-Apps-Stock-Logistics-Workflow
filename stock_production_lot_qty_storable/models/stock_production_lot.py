8a6296b21a866c32fa4ba0e6fd235a866143bad5
Laszlo Krajnikovszkij <laszlo.lk@protonmail.com>
2024-03-11 14:28:50 +0100

    feat: migrate stock_production_lot_qty_storable

---
+++ stock_production_lot_qty_storable/models/stock_production_lot.py
@@ -0,0 +1,9 @@
+from odoo import _, api, fields, models
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class StockProductionLot(models.Model):
+    _inherit = 'stock.lot'
+
+    product_qty = fields.Float(store=True, readonly=True)
