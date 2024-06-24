9fdb02990794bffa2f468556d0689f99100d6e42
Laszlo Krajnikovszkij <laszlo.lk@protonmail.com>
2024-06-24 13:48:14 +0200

    feat: init module migration

---
+++ stock_critical_forecast_link_replenish/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Stock Critical Forecast Link Replenish",
+    "summary": """
+        Calculate action date from active orderpoint rules.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://www.mint-system.ch",
+    "category": "Inventory",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["stock_critical_forecast"],
+    "data": ["views/critical_forecast.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}
---
+++ stock_critical_forecast_link_replenish/models/critical_forecast.py
@@ -0,0 +1,56 @@
+import logging
+
+from odoo import fields, models
+
+_logger = logging.getLogger(__name__)
+from dateutil import relativedelta
+
+
+class CriticalForecast(models.Model):
+    _inherit = "critical.forecast"
+
+    def _compute_critical_date(self, product_id, replenish_data):
+        """Check if active orderpoint for this product exists."""
+        res = super()._compute_critical_date(product_id, replenish_data)
+        if not res:
+            oderpoint_id = self.env["stock.warehouse.orderpoint"].search(
+                [("product_id", "=", product_id.id), ("qty_to_order", ">", 0.0)],
+                limit=1,
+            )
+            if oderpoint_id:
+                # Action date is always today, therefore critical date is today plus rplenish delay.
+                replenish_delay = self._compute_replenish_delay(product_id)
+                return fields.Date.today() + relativedelta.relativedelta(
+                    days=replenish_delay
+                )
+        return res
+
+    def _get_order_data(self, data=[], product_ids=[]):
+        """Add products with active orderopint to data list."""
+
+        # Lookup orderpoints with reorder filter
+        oderpoint_ids = self.env["stock.warehouse.orderpoint"].search(
+            [("qty_to_order", ">", 0.0)]
+        )
+
+        for orderpoint in oderpoint_ids.filtered(
+            lambda o: o.product_id.id not in product_ids
+        ):
+            replenish_data = self.env[
+                "report.stock.report_product_product_replenishment"
+            ]._get_report_data([orderpoint.product_tmpl_id.id])
+            data.append(
+                self._prepare_report_line(orderpoint.product_id, replenish_data)
+            )
+            product_ids.append(orderpoint.product_id.id)
+
+        return data, product_ids
+
+    def action_product_replenish(self):
+        """Open product replenish view."""
+        self.ensure_one()
+        action = self.env["ir.actions.actions"]._for_xml_id(
+            "stock.action_orderpoint_replenish"
+        )
+        action["domain"] = [("product_id", "=", self.product_id.id)]
+        return action
