f1414828467aa4fb73ad998d8ed360d0753641fd
Laszlo Krajnikovszkij <laszlo.lk@protonmail.com>
2024-07-09 10:45:41 +0200

    feat: upgrade module, add test instructions

---
+++ stock_move_sale_order_name/models/stock_move_line.py
@@ -0,0 +1,15 @@
+import logging
+
+from odoo import models
+
+_logger = logging.getLogger(__name__)
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    def _prepare_procurement_values(self, group_id=False):
+        vals = super()._prepare_procurement_values(group_id=group_id)
+        if self.name:
+            vals["product_description_variants"] = self.name
+        return vals
