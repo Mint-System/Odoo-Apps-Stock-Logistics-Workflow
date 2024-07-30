b15c3e768d349bfa7a933c3f5cdfc52e092bc3f2
Janik von Rotz <login@janikvonrotz.ch>
2024-07-30 13:22:17 +0200

    feat: use forecasted report data to calculate orderpoint date

--- stock_critical_forecast_link_replenish/models/critical_forecast.py
+++ stock_critical_forecast_link_replenish/models/critical_forecast.py
@@ -2,6 +2,8 @@ import logging
 
 from dateutil import relativedelta
 
+from datetime import timedelta, datetime
+
 from odoo import fields, models
 
 _logger = logging.getLogger(__name__)
@@ -10,23 +12,49 @@ _logger = logging.getLogger(__name__)
 class CriticalForecast(models.Model):
     _inherit = "critical.forecast"
 
+    def _compute_orderpoint_date(self, product_id, product_min_qty):
+        """
+        Read data from the forecast report.
+        Extract orderpoint date from read group result.
+        """
+        today = fields.Datetime.today()
+        to_date = today + timedelta(days=90)
+        forecast_report = self.env['report.stock.quantity'].read_group(
+            domain=[
+                ('date', '>=', today),
+                ('date', '<=', to_date),
+                ('product_id', '=', product_id.id),
+                ('company_id', '=', self.env.company.id),
+                ('product_qty', '<', product_min_qty),
+            ],
+            fields=['date'],
+            groupby=['date:day'],
+            orderby='date',
+            limit=1,
+            lazy=False
+        )
+
+        orderopint_date = False
+        if forecast_report:
+            orderopint_date = datetime.strptime(forecast_report[0]['__range']['date:day']['from'], '%Y-%m-%d').date()
+        return orderopint_date
+
     def _compute_critical_date(self, replenish_data):
-        """Check if active orderpoint for this product exists."""
-        # Call the base method first
-        res = super()._compute_critical_date(replenish_data)
-        if not res:
-            product_id = replenish_data["product_templates"][0].product_variant_id
-            orderpoint_id = self.env["stock.warehouse.orderpoint"].search(
-                [("product_id", "=", product_id.id), ("qty_to_order", ">", 0.0)],
-                limit=1,
-            )
-            if orderpoint_id:
-                # Action date is always today, therefore critical date is today plus replenish delay.
-                replenish_delay = self._compute_replenish_delay(product_id)
-                return fields.Date.today() + relativedelta.relativedelta(
-                    days=replenish_delay
-                )
-        return res
+        """
+        Compute critical date based on orderpoint and forecast report.
+        """
+        critical_date = super()._compute_critical_date(replenish_data)
+
+        product_id = replenish_data["product_templates"][0].product_variant_id
+        orderpoint_id = self.env["stock.warehouse.orderpoint"].search(
+            [("product_id", "=", product_id.id), ("product_min_qty", ">", 0.0)],
+            limit=1,
+        )
+        if orderpoint_id:
+            orderpoint_date =self._compute_orderpoint_date(product_id, orderpoint_id.product_min_qty)            
+            if (orderpoint_date and not critical_date) or (orderpoint_date < critical_date.date()):
+                return orderpoint_date
+        return critical_date
 
     def _get_order_data(self, data=[], product_ids=[]):
         """Add products with active orderpoint to data list."""
--- stock_critical_forecast_link_replenish/tests/TEST_INSTRUCTIONS.rst
+++ stock_critical_forecast_link_replenish/tests/TEST_INSTRUCTIONS.rst
@@ -1,6 +1,32 @@
-- Open the Inventory app
+Add orderpoint:
 - Open the product "Tischplatte"
 - Create order point with minimum quantity 3
 - Under Operations menu click "Calculate Critical Forecast"
 - Under Reporting menu open the Critical Forecast
 - Check if the product shows up with a critical date today
+
+Add replenish delay:
+- Open the product "Tischplatte"
+- Add produce delay of 7 days
+- Under Operations menu click "Calculate Critical Forecast"
+- Under Reporting menu open the Critical Forecast
+- Check if the product shows up with a actjion date minus 7 days
+
+
+Update manufacturing:
+- Open "WH/MO/00002" and set date + 7 days
+- Under Operations menu click "Calculate Critical Forecast"
+- Under Reporting menu open the Critical Forecast
+- Check if product "Tischbein" is shown with today + 7 days
+
+Add stock:
+- Open the product "Tischbein"
+- Set quantity on hand to 4
+- Under Operations menu click "Calculate Critical Forecast"
+- Under Reporting menu open the Critical Forecast 
+- Check if product "Tischbein" is not shown
+- Open the product "Tischbein"
+- Create order point with minimum quantity 1
+- Under Operations menu click "Calculate Critical Forecast"
+- Under Reporting menu open the Critical Forecast 
+- Check if product is shown
