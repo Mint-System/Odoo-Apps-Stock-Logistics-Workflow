2e6bdd4c14453669c9d5dabade13bf2f89c98c2f
Ulrich Kilian <uk@science-and-more.de>
2025-07-10 18:03:06 +0200

    feat(stock_move_line_position): add position numbers to moves of pickings without origin

--- stock_move_line_position/models/stock_move.py
+++ stock_move_line_position/models/stock_move.py
@@ -8,7 +8,9 @@ _logger = logging.getLogger(__name__)
 class StockMove(models.Model):
     _inherit = "stock.move"
 
-    position = fields.Integer("Pos", compute="_compute_get_position")
+    #position = fields.Integer("Pos", compute="_compute_get_position")
+    position = fields.Integer("Pos")
+
 
     def _compute_get_position(self):
         for move in self:
@@ -18,3 +20,5 @@ class StockMove(models.Model):
                 move.position = move.purchase_line_id.position
             else:
                 move.position = 0
+
+    
