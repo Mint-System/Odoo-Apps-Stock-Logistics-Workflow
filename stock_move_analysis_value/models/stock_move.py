56e1ea9d9bf31108c4d3a0a7eb4b825f958b0a7c
Ulrich Kilian <uk@science-and-more.de>
2025-08-05 17:11:02 +0200

    feat(stock_move_analysis_value): calculated planned costs as product standard price times demand

--- stock_move_analysis_value/models/stock_move.py
+++ stock_move_analysis_value/models/stock_move.py
@@ -6,7 +6,22 @@ class StockMove(models.Model):
     _inherit = 'stock.move'
 
     product_standard_price = fields.Float(
-        string="Standard Cost",
-        related='product_id.standard_price',
+        string="Product Standard Cost",
+        # related='product_id.standard_price',
+        compute="_compute_product_costs",
         store=True
     )
+
+    planned_costs = fields.Float(
+        string="Planned Cost (Demand x Standard Price)",
+        compute="_compute_product_costs",
+        store=True
+    )
+
+    @api.depends('product_id', 'product_uom_qty')
+    def _compute_product_costs(self):
+        for move in self:
+            standard_price = move.product_id.standard_price
+            move.product_standard_price = standard_price or 0.0
+            move.planned_costs = move.product_uom_qty * standard_price
+
