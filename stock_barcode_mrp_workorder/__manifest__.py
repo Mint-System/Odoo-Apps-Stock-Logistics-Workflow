11e4190605f6642c89bceccb6c01058324657236
Ulrich Kilian <uk@science-and-more.de>
2025-09-02 15:10:45 +0200

    feat(stock_barcode_mrp_workorder): init module

---
+++ stock_barcode_mrp_workorder/__manifest__.py
@@ -0,0 +1,17 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock Barcode Mrp Workorder",
+    "summary": """
+        Access work order by scanning the barcode of a manufacturing order.
+    """,
+    "author": "Mint System GmbH",
+    "website": "https://www.mint-system.ch",
+    "category": "Manufacturing",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["stock_barcode", "mrp_workorder"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}
