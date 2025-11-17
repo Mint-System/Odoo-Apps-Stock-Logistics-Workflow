f5007001387881968e69e9fd8cb6fcd065377af4
Ulrich Kilian <uk@science-and-more.de>
2025-11-17 11:24:30 +0100

    feat(stock_picking_responsible): migrate

---
+++ stock_picking_responsible/models/stock_move.py
@@ -0,0 +1,16 @@
+import logging
+
+from odoo import models
+
+_logger = logging.getLogger(__name__)
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    def _get_new_picking_values(self):
+        vals = super()._get_new_picking_values()
+        user_id = self.sale_line_id.order_id.user_id
+        if user_id:
+            vals.update({"user_id": user_id.id})
+        return vals
---
+++ stock_picking_responsible/tests/TEST_INSTRUCTIONS.rst
@@ -0,0 +1,4 @@
+- Open Sales > Orders > Quotations
+- Create a new quotation for Deco Addict with product [FURN_7800] Desk Combination
+- Confirm the Sales Order and open the Delivery
+- In the Additional Info tab the Responsible person must be same as the Salesperson on the Sales Order
