ee7d8d2d2b29740a29e4fcc71149e0d6267e5cbb
Ulrich Kilian <uk@sdcience-and-more.de>
2026-03-24 14:31:41 +0100

    feat(stock_move_lots_indicator): improve indicator

--- stock_move_lots_indicator/__manifest__.py
+++ stock_move_lots_indicator/__manifest__.py
@@ -10,7 +10,7 @@
     "development_status": "Production/Stable",
     "version": "18.0.1.0.0",
     "license": "AGPL-3",
-    "depends": ["stock"],
+    "depends": ["stock","stock_move_show_tracking"],
     "data": [
         "views/stock_move_views.xml"
     ],
--- stock_move_lots_indicator/models/stock_move.py
+++ stock_move_lots_indicator/models/stock_move.py
@@ -17,10 +17,14 @@ class StockMove(models.Model):
     @api.depends('move_line_ids', 'move_line_ids.lot_name')
     def _compute_lot_generated_status(self):
         for move in self:
-            move_lines = move.move_line_ids
-            if not move_lines:
-                move.lot_generated_status = "⚠️"
-            elif all(line.lot_name for line in move_lines):
-                move.lot_generated_status = "✅"
+            product = move.product_id
+            if product.tracking == "none":
+                move.lot_generated_status = "-"
             else:
-                move.lot_generated_status = "❌"
+                move_lines = move.move_line_ids
+                if not move_lines:
+                    move.lot_generated_status = "-"
+                elif all(line.lot_name for line in move_lines):
+                    move.lot_generated_status = "✅"
+                else:
+                    move.lot_generated_status = "❌"
