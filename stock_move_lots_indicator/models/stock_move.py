2031f8406402e73afa72111b6caa09a9a7bb4e66
Ulrich Kilian <uk@sdcience-and-more.de>
2026-04-15 09:15:20 +0200

    feat(stock_move_lots_indicator): remove tooltip

--- stock_move_lots_indicator/models/stock_move.py
+++ stock_move_lots_indicator/models/stock_move.py
@@ -13,31 +13,6 @@ class StockMove(models.Model):
         readonly=True
     )
 
-    lot_generated_status_tooltip = fields.Char(
-        compute="_compute_lot_generated_status",
-        store=False
-    )
-
-
-    # @api.depends('move_line_ids', 'move_line_ids.lot_name', 'move_line_ids.lot_id')
-    # def _compute_lot_generated_status(self):
-    #     for move in self:
-    #         product = move.product_id
-    #         if product.tracking == "none":
-    #             move.lot_generated_status = "-"
-    #         else:
-    #             _logger.warning(f"product: {product}")
-    #             move_lines = move.move_line_ids
-    #             for line in move_lines:
-    #                 _logger.warning(f"lot name: {line.lot_name}, lot id: {line.lot_id}")
-    #             if not move_lines:
-    #                 move.lot_generated_status = "-"
-    #             elif all(line.lot_name or line.lot_id for line in move_lines):
-    #                 move.lot_generated_status = "✅"
-    #             else:
-    #                 move.lot_generated_status = "❌"
-
-
 
     @api.depends('move_line_ids', 'move_line_ids.lot_name', 'move_line_ids.lot_id')
     def _compute_lot_generated_status(self):
@@ -95,3 +70,5 @@ class StockMove(models.Model):
                 move.lot_generated_status = "⚠️"
             else:
                 move.lot_generated_status = "✅"
+
+
