39ab29ab8c525b63ef7bf5d59903b9e54cbf1373
Ulrich Kilian <uk@sdcience-and-more.de>
2026-07-27 08:50:12 +0200

    fix(stock_move_lot_quantity): picking product_qty

--- stock_move_lot_quantity/models/stock_move_line.py
+++ stock_move_lot_quantity/models/stock_move_line.py
@@ -11,7 +11,7 @@ class StockMoveLine(models.Model):
     @api.onchange('lot_id')
     def _onchange_lot_id_fill_quantity(self):
         for line in self:
-            if not line.lot_id or line.product_id.tracking == 'lot':
+            if not line.lot_id or line.product_id.tracking != 'lot':
                 continue
             if line.picking_id.picking_type_id.pick_all_lot_qty:
                 line.quantity = line.lot_id.product_qty
